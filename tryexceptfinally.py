cd4f64919f497a7cb62f4a0ec1b01563126d0e83
harry oslislo <oslislo1@hotmail.com>
2017-08-06 10:34:46 -0400

    added URL for exceptions

--- tryexceptfinally.py
+++ tryexceptfinally.py
@@ -18,6 +18,8 @@ try:
 except ZeroDivisionError as detail:
     print 'this is the run-time error handled in the except block:', detail
 
+# Python exceptions
+# https://docs.python.org/2/library/exceptions.html
 '''
 Enter a number and I'll square it: "fgh"
 dude. I asked you for a number and fgh is not a number.
